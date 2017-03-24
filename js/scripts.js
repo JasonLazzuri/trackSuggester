d6d77cf04d503130c5041b1db1b36e61eddad92f
JasonLazzuri <jason.Lazzuri@gmail.com>
2017-03-24 11:32:10 -0700

    add business to get a result

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+var create1 = function(create,companies,types,important,guess){
+  return create + companies + types + important + guess;
+};
+
+
 $(document).ready(function() {
   $("#form").submit(function(event) {
 
@@ -6,11 +11,26 @@ $(document).ready(function() {
       var types = parseInt($("#types").val());
       var important = parseInt($("#important").val());
       var guess = parseInt($("#guess").val());
+      var createResult = create1(create,companies,types,important,guess);
 
 
-      if (create === 1 && companies === 1 && types === 1 && important === 1 && guess === 1) {
-        alert("C# looks awesome for you");
+      if (createResult > 9){
+        alert("Ruby looks like a great choice!");$("#rubyChoice").show();
       }
+
+
+      // if ((create === 1 && companies === 1 && types === 1 && important === 1 && guess === 1)||
+      //     (create === 1 && companies === 1 && types === 1 && important > 1 && guess > 1 )||
+      //     (create > 1 && companies > 1 && types === 1 && important === 1 && guess === 1 )
+      //     (creat > 1)) {
+      //   alert("C# looks awesome for you"); $("#cChoice").show();
+      //
+      // } else if
+      // ((create === 2 && companies === 2 && types === 2 && important === 2 && guess === 2)||
+      // (create === 2 && companies === 2 && types === 2 && important !== 2 && guess !== 2)||
+      // (create !== 2 && companies !== 2 && types === 2 && important === 2 && guess === 2 )){
+      //   alert("Ruby looks awesome for you"); $("#rubyChoice").show();
+      // }
     event.preventDefault()
   });
 });
