72780fe28a5c09bfb1cc2d5d30cb3f8142215569
JasonLazzuri <jason.Lazzuri@gmail.com>
2017-03-24 09:42:57 -0700

    created js and almost finished html side of the code

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("#form").submit(function(event) {
+
+      var create = $("#create").val();
+      var companies = $("#companies").val();
+      var types = $("#types").val();
+      var important = $("#important").val();
+      var guess = $("#guess").val();
+
+
+
+
+
+    event.preventDefault()
+  });
+});
