58d4f241defd130f578194885a99759e001ed611
JasonLazzuri <jason.Lazzuri@gmail.com>
2017-03-24 10:32:40 -0700

    fix major isses where tags were not correct

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   $("#form").submit(function(event) {
 
-      var create = $("#create").val();
-      var companies = $("#companies").val();
-      var types = $("#types").val();
-      var important = $("#important").val();
-      var guess = $("#guess").val();
+      var create = parseInt($("#create").val());
+      var companies = parseInt($("#companies").val());
+      var types = parseInt($("#types").val());
+      var important = parseInt($("#important").val());
+      var guess = parseInt($("#guess").val());
 
 
-
-
-
+      if (create === 1 && companies === 1 && types === 1 && important === 1 && guess === 1) {
+        alert("C# looks awesome for you");
+      }
     event.preventDefault()
   });
 });
